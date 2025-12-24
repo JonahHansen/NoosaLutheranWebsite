08a54756721b3a4f3864bb4efacc44b678e07102
Jonah Hansen <jonah.hansen@anu.edu.au>
2025-12-24 03:22:53 +0100

    Remodeled to be html only

--- base_content/compile.py
+++ base_content/compile.py
@@ -1,8 +1,15 @@
+from glob import glob
+
 def fill_template(template_text, title, current, contents_text):
     header,footer = template_text.split('!!main!!')
 
     new_text = template_text.replace('!!title!!', title
         ).replace('!!main!!', contents_text)
+    
+    ## REPLACE FILENAMES
+    bulletin_fname = glob("../content/bulletin/*")[0].split("..")[1][1:]
+    theme_fname = glob("../content/theme/*")[0].split("..")[1][1:]
+    new_text = new_text.replace('!!THEME!!',theme_fname).replace('!!BULLETIN!!',bulletin_fname)
 
     for tab in ["!!current_home!!","!!current_cong!!","!!current_worship!!","!!current_ministry!!","!!current_events!!","!!current_contact!!"]:
         if tab == current:
@@ -10,15 +17,17 @@ def fill_template(template_text, title, current, contents_text):
         else:
             new_text = new_text.replace(tab,"")
 
-    if title == 'Contact Form Response':
-        footer = "'" + footer.replace("'","\\'") + "'"
-
-        new_text = new_text.replace('!!footer!!',footer)
+    if title == 'Weddings':
+        slideshow_text = slideshow_filler("../content/wedding_slideshow")
+        new_text = new_text.replace("!!SLIDESHOW!!",slideshow_text)
+    if title == 'Our Congregation':
+        slideshow_text = slideshow_filler("../content/congregation_slideshow")
+        new_text = new_text.replace("!!SLIDESHOW!!",slideshow_text)
 
     return new_text
 
 def make_template(name: str, current: str, title: str):
-    with open('_template.php') as f_template:
+    with open('_template.html') as f_template:
         template_text = f_template.read()
         with open(f'_{name}', encoding="utf8") as f_contents:
             with open(f'../{name}', 'w', encoding="utf8") as f_to:
@@ -29,24 +38,55 @@ def make_template(name: str, current: str, title: str):
                     current,
                     f_contents.read()))
 
+def slideshow_filler(folder):
+
+    txt = """<div>
+              <h2 class="imgholder"><div class="slideshow-container">
+          """
+
+    files = glob(folder+"/*")
+    total = len(files)
+    for i,f in enumerate(files):
+        f_name = f.split("..")[1][1:]
+        elem = f"""<div class='mySlides fade'>
+               <div class='numbertext'>{i+1} / {total}</div>
+               <h2 class='imgholder', style='height:30vw'>
+               <img src={f_name} style='max-height:100%'></h2>
+               </div>"""
+        txt+=elem
+
+    txt+="""<!-- Next and previous buttons -->
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+              </div>
+              <div style="text-align:center;margin-top:-2.2em">
+               """
+
+    for i in range(total):
+        elem = f"<span class='dot' onclick='currentSlide({i+1})'></span>"
+        txt+=elem
+
+    txt+="</div>"
+
+    return txt
+
+
 names = ['childsafe.html',
-         'congregation.php',
+         'congregation.html',
          'contact.html',
-         'contact-form-mailer.php',
          'facilities.html',
          'faithink.html',
-         'index.php',
+         'index.html',
          'lutheran.html',
          'ministry.html',
          'purpose.html',
-         'events.php',
-         'weddings.php',
-         'worship.php'
+         'events.html',
+         'weddings.html',
+         'worship.html'
         ]
 
 currents = ["!!current_ministry!!",
 "!!current_cong!!",
-"!!current_contact!!",
 "!!current_contact!!",
 "!!current_events!!",
 "!!current_ministry!!",
@@ -61,7 +101,6 @@ currents = ["!!current_ministry!!",
 titles = ['Child Safe Standards',
          'Our Congregation',
          'Contact Us',
-         'Contact Form Response',
          'Facilities Hire',
          'Faith Inkubators',
          'Good Shepherd Lutheran Church - Noosa',
